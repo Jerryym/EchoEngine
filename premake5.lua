c3a9f401a801f1caba220f18b6dc7133232e1e89
Jerry_yang <1622595386@qq.com>
2023-02-02 23:11:21 +0800

    Add premake script

---
+++ premake5.lua
@@ -0,0 +1,97 @@
+workspace "Echo"
+    architecture "x64"
+    configurations { "Debug", "Release", "Dist" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Echo"
+    location "Echo"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")   --输出目录
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")  --中间目录
+
+    files
+    {
+        "%{prj.name}/src/**.h";
+        "%{prj.name}/src/**.cpp";
+    }
+
+    includedirs
+    {
+		"%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+		systemversion "latest"  --windows sdk 版本
+
+        defines 
+		{ 
+			"ECHO_PLATFORM_WINDOWS",
+			"ECHO_BUILD_DLL"
+		}
+
+        -- 复制dll到Sandbox.exe文件夹中
+        postbuildcommands { "{COPYDIR} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox" }
+
+    filter "configurations:Debug"
+        defines "ECHO_DEBUG"
+		symbols "On"
+
+    filter "configurations:Release"
+        defines "ECHO_RELEASE"
+		symbols "On"
+
+    filter "configurations:Dist"
+        defines "ECHO_DIST"
+		symbols "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")   --输出目录
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")  --中间目录
+
+    files
+    {
+        "%{prj.name}/src/**.h";
+        "%{prj.name}/src/**.cpp";
+    }
+
+    includedirs
+    {
+        "Echo/vendor/spdlog/include",
+        "Echo/src"
+    }
+
+    links
+    {
+        "Echo"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+		systemversion "latest"  --windows sdk 版本
+
+        defines 
+		{ 
+			"ECHO_PLATFORM_WINDOWS",
+		}
+
+    filter "configurations:Debug"
+        defines "ECHO_DEBUG"
+		symbols "On"
+
+    filter "configurations:Release"
+        defines "ECHO_RELEASE"
+		symbols "On"
+
+    filter "configurations:Dist"
+        defines "ECHO_DIST"
+		symbols "On"
