6a698113fd382f41d4c4cda988782a1ffa89066c
Jerry_yang <1622595386@qq.com>
2023-03-12 20:51:22 +0800

    Added a singleton application object in Application class

--- Echo/src/Echo/Application.h
+++ Echo/src/Echo/Application.h
@@ -18,11 +18,24 @@ namespace Echo {
 		/// @param event 
 		void OnEvent(Event& event);
 
-		/// @brief 运行
+		/// @brief 运行应用程序
 		void Run();
 
+		/// @brief 从层栈的第一个元素添加一个层级
+		/// @param layer 
 		void PushLayer(Layer* layer);
+
+		/// @brief 添加一个层到栈
+		/// @param layer 
 		void PushOverlay(Layer* layer);
+
+		/// @brief 获取当前应用程序
+		/// @return 当前应用程序指针
+		inline static Application& GetApplication() { return *s_Instance; }
+		
+		/// @brief 获取当前活动窗口
+		/// @return 当前活动窗口指针
+		inline Window& GetWindow() { return *m_Window; }
 
 	private:
 		/// @brief 关闭窗口
@@ -33,6 +46,9 @@ namespace Echo {
 		std::unique_ptr<Window> m_Window;	//应用显示窗口
 		bool m_bRunning = true;				//运行状态的控制
 		LayerStack m_LayerStack;			//层栈
+
+		//应用程序单例指针（程序运行时，当且只能一个应用程序存在）
+		static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
