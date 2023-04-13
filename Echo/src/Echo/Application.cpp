9e695f8aedccb3fbd298fc15e42f7403bff65597
Jerry_yang <1622595386@qq.com>
2023-04-13 22:45:30 +0800

    Added Shader

--- Echo/src/Echo/Application.cpp
+++ Echo/src/Echo/Application.cpp
@@ -19,6 +19,52 @@ namespace Echo {
 
 		m_pImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_pImGuiLayer);
+
+		glGenVertexArrays(1, &m_VertexArray);
+		glBindVertexArray(m_VertexArray);
+
+		glGenBuffers(1, &m_VertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+		float vertices[3 * 3] = {
+			-0.5f, -0.5f, 0.0f,
+			0.0f,   0.5f, 0.0f,
+			0.5f,  -0.5f, 0.0f
+		};
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		glGenBuffers(1, &m_IndexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+		unsigned int indices[3] = { 0,1,2 };
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STREAM_DRAW);
+
+		std::string vertexShader = R"(
+			#version 330 core
+			layout(location = 0) in vec3 a_Position;
+			out vec3 v_Position;
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string fragmentShader = R"(
+			#version 330 core
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+			};
+		)";
+
+		m_Shader.reset(new Shader(vertexShader, fragmentShader));
 	}
 
 	Application::~Application()
@@ -50,6 +96,10 @@ namespace Echo {
 		{
 			glClearColor(0.1f, 0.1f, 0.1f, 1.f);
 			glClear(GL_COLOR_BUFFER_BIT);
+			
+			m_Shader->Bind();
+			glBindVertexArray(m_VertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
 			// 遍历层栈，实现各层更新
 			for (Layer* layer : m_LayerStack)
--- Echo/src/Echo/Application.h
+++ Echo/src/Echo/Application.h
@@ -8,6 +8,8 @@
 
 #include "ImGui/ImGuiLayer.h"
 
+#include "Renderer/Shader.h"
+
 namespace Echo {
 
 	class ECHO_API Application
@@ -49,6 +51,9 @@ namespace Echo {
 		ImGuiLayer* m_pImGuiLayer;			//ImGui UI层
 		bool m_bRunning = true;				//运行状态的控制
 		LayerStack m_LayerStack;			//层栈
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;	//着色器
 
 		//应用程序单例指针（程序运行时，当且只能一个应用程序存在）
 		static Application* s_Instance;
