5ddebb5325c56e281f110d3a424553cc01f9747b
Jerry_yang <1622595386@qq.com>
2024-01-05 22:53:57 +0800

    优化Nest框架中的Application

--- Nest/src/Core/NestCore.h
+++ Nest/src/Core/NestCore.h
@@ -10,8 +10,7 @@
 #endif // NEST_STATIC_LINK
 
 #ifdef NEST_ENABLE_ASSERTS
-	#define NEST_ASSERT(X, ...) { if(!(x)) { ECHO_ERROR("Assertion Failed: {0}", _VA_ARGS_); __debugbreak(); } }
-	#define NEST_CORE_ASSERT(X, ...) { if(!(x)) { ECHO_CORE_ASSERT("Assertion Failed: {0}", _VA_ARGS_); __debugbreak(); } }
+	#define NEST_CORE_ASSERT(X, ...) { if(!(x)) { NEST_CORE_ASSERT("Assertion Failed: {0}", _VA_ARGS_); __debugbreak(); } }
 #else
 	#define NEST_ASSERT(X, ...)
 	#define NEST_CORE_ASSERT(X, ...) 
