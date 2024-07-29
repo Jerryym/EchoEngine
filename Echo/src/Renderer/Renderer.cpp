22d7436fe749d4b94b852ed7670ee999692187fb
Jerry_yang <1622595386@qq.com>
2024-07-29 22:41:42 +0800

    Implement perspective projection

--- Echo/src/Renderer/Renderer.cpp
+++ Echo/src/Renderer/Renderer.cpp
@@ -3,13 +3,17 @@
 
 #include "Platform/OpenGL/OpenGLShader.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace Echo {
 
 	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
 
 	void Renderer::BeginScene(Camera& camera)
 	{
-		m_SceneData->ViewProjectionMatrix = camera.GetProjectcionMat();
+		m_SceneData->ViewProjectionMatrix = camera.GetProjectcionMat() * 
+			glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f)) * 
+			glm::rotate(glm::mat4(1.0f), glm::radians(-60.0f), glm::vec3(1.0f, 0.0f, 0.0f));;
 	}
 
 	void Renderer::EndScene()
